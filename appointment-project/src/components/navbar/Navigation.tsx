23cf51ebc1be1ba47c8bbea7284ecaa020423e99
iZackk__26 <newxstorecr@gmail.com>
2024-12-20 21:31:30 -0600

    feat: finished navbar and header

--- appointment-project/src/components/navbar/Navigation.tsx
+++ appointment-project/src/components/navbar/Navigation.tsx
@@ -5,13 +5,22 @@ import { Link } from 'react-router-dom'
 export default function Navigation() {
     return (
         <nav className="flex items-center gap-6">
-          {/* Home Link */}
-          <Link
-            to="/home"
-            className="text-sm md:text-lg font-semibold text-gray-800 dark:text-gray-200 hover:underline"
-          >
-          </Link>
-    
+            {/* LogIn Link */}
+            <Link
+                to="/home"
+                className="text-sm md:text-sm font-semibold text-black"
+            >
+                Login
+            </Link>
+
+            {/* SignUp Link */}
+            <Link
+                to="/signup"
+                className="text-sm md:text-sm font-semibold text-black"
+            >
+                Sign Up
+            </Link>
+
         </nav>
-      );
+    );
 }
