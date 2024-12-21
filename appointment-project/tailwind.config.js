dd9da869e29f73ad9163cd8064fe5851ae100a8e
akhoz <adrian25092005@gmail.com>
2024-12-20 21:30:07 -0600

    feat: Added Home/Landing Page first approach

--- appointment-project/tailwind.config.js
+++ appointment-project/tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Light Theme
+        lightPrimary: "hsl(11, 100%, 100%)",
+        lightSecondary: "hsl(222, 35.7%, 11%)",
+      },
+    },
   },
   plugins: [],
-}
+};
